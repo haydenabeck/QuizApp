947e51aa8a39a627f6c624daaaa0ed6357441463
Hayden Beck <haydenabeck@gmail.com>
2021-04-06 17:51:06 -0500

    added html boiler plate, added blank css, added javascript questions array

---
+++ script.js
@@ -0,0 +1,47 @@
+// All questions and answers for quiz
+var questions = [
+    {
+        prompt: "What day is it?\n(a) Sunday\n(b) Monday\n(c) Tuesday",
+        answer: "a"
+    },
+    {
+        prompt: "Is it simply better ?\n(a) No\n(b) yeah.\n(c) Do explain.",
+        answer: "c"
+    },
+    {
+        prompt: "Why do cats meow?\n(a) Hungry bellies\n(b) ALERT!!!!\n(c) They know that a great silence is always a pleasure",
+        answer: "b"
+    },
+    {
+        prompt: "How are you?\n(a) a good answer\n(b) exciting adventure\n(c) start to fridge.",
+        answer: "a"
+    },
+    {
+        prompt: "What color is my baby's hair?\n(a) blue\n(b) baby hair\n(c) small baby hair",
+        answer: "a"
+    },
+    {
+        prompt: "When you look at today?\n(a) The belly a ruckus\n(b) FIBER\n(c) There",
+        answer: "c"
+    },
+    {
+        prompt: "What is?\n(a) that\n(b) this\n(c) robot voice",
+        answer: "a"
+    },
+    {
+        prompt: "Are there different types of animals?\n(a) Yes\n(b) No\n(c) No",
+        answer: "c"
+    },
+    {
+        prompt: "What is a your mind?\n(a) dance\n(b) Meat\n(c) tree",
+        answer: "a"
+    },
+    {
+        prompt: "What have you noticed today?\n(a) There are huge heads on Easter Island\n(b) The candies were tasty\n(c) just a burger",
+        answer: "a"
+    },
+]
+
+var score = 0;
+
+
